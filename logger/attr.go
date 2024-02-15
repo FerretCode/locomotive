6fcf65ea14e1573ca30223fd6dcfb4c880c854f0
Brody Over <10548119+brody192@users.noreply.github.com>
2024-02-14 19:39:06 -0500

    add logger

---
+++ logger/attr.go
@@ -0,0 +1,20 @@
+package logger
+
+import (
+	"log/slog"
+	"strings"
+)
+
+func ErrAttr(err error) slog.Attr {
+	return slog.String("err", strings.TrimSpace(err.Error()))
+}
+
+func ErrorsAttr(errors ...error) slog.Attr {
+	stringErrors := []string{}
+
+	for _, err := range errors {
+		stringErrors = append(stringErrors, strings.TrimSpace(err.Error()))
+	}
+
+	return slog.Any("errors", stringErrors)
+}
